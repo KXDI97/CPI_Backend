979016b75d881feb5326a1776b7e6dd674e54c59
KXDI <solorzanobryams@gmail.com>
2025-04-08 15:50:17 -0500

    version 1.1

--- APIUsuarios/Program.cs
+++ APIUsuarios/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using APIUsuarios.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Agrega el servicio CORS
@@ -14,6 +18,10 @@ builder.Services.AddCors(options =>
 // OpenAPI (Swagger)
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQL")));
+
+
 var app = builder.Build();
 
 // Usa CORS antes del mapeo de rutas
@@ -24,6 +32,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapGet("/usuarios", async (AppDbContext db) =>
+{
+    var usuarios = await db.Usuarios.ToListAsync();
+    return Results.Ok(usuarios);
+});
 
 
 app.UseHttpsRedirection();
